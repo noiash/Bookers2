9234037d0fcf9c3dde26bcee1da7d9b2f367c316
noiash <noinoi51267@gmail.com>
2021-09-18 13:48:47 +0000

    [Update]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cbf438d030294a497b1f31879cced0eeb89da117976c4bbe6c66496504be46530d35316784f2a50a98541edf7a4396e1c04d1d553c316a647ae7b6802edf4546'
